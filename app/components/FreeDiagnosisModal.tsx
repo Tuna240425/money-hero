c0e5a209784a61a36fdc2b64f758d344688f0a82
Aurum Manager <ynalee1130@gmail.com>
2025-08-11 13:15:41 +0900

    변경사항 저장

--- app/components/FreeDiagnosisModal.tsx
+++ app/components/FreeDiagnosisModal.tsx
@@ -100,8 +100,6 @@ export default function FreeDiagnosisModal({ open, setOpen }: { open: boolean, s
               </div>
             </div>
 
-            // FreeDiagnosisModal.tsx에서 채권 금액 선택 부분 수정
-
             <div className="grid gap-2">
             <Label>채권 금액</Label>
             <Select name="amount" defaultValue="1천만~3천만">
